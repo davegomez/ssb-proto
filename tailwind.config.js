47f3ee8424ebbba2b88c9094a7704f0270068488
David Gomez <dg.dave@hey.com>
2022-01-10 16:01:37 -0500

    Setup base font family (Roboto)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,21 @@ const fromRoot = (p) => path.join(__dirname, p);
 module.exports = {
   content: [fromRoot('./app/**/*.+(js|ts|tsx|mdx|md)')],
   theme: {
+    fontFamily: {
+      sans: [
+        'Roboto',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe\\ UI',
+        'Oxygen',
+        'Ubuntu',
+        'Cantarell',
+        'Fira\\ Sans',
+        'Droid\\ Sans',
+        'Helvetica\\ Neue',
+        'sans-serif',
+      ],
+    },
     extend: {},
   },
   plugins: [],
